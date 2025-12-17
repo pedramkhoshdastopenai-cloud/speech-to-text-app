50fa64ca52931ece583c6ba01e564d157c6b431b
NIT computer <pedramkhoshdast@gmail.com>
2025-12-17 14:55:56 +0330

    bug fix

--- src/app/api/stt/route.ts
+++ src/app/api/stt/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
+import OpenAI from 'openai';
 import ffmpeg from 'fluent-ffmpeg';
 import ffmpegPath from 'ffmpeg-static';
 
